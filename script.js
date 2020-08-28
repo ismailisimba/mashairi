1a515da423dbe72d1d5ced6050af783835b70e0a
Ismail Amir <Ismaili@TIM.ISTAFRICA.COM>
2020-08-28 03:56:18 +0300

    recreated onchange webpage edit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const width = window.screen.width;
 const localFunc = {
        setScene: function () {
 
@@ -8,21 +9,71 @@ const localFunc = {
             throwawayNode = null;
       
             let newMainDiv = document.createElement("div");
-            newMainDiv.style.fontSize = "12px";
-            newMainDiv.style.width = "85%";
+            newMainDiv.style.fontSize = "24px";
+            newMainDiv.style.width = "90%";
             newMainDiv.style.height = "500px";
-            newMainDiv.style.borderRadius = "2px";
+            newMainDiv.style.borderRadius = "1px";
             newMainDiv.style.borderStyle = "solid";
-            newMainDiv.style.borderColor = "black";
-            newMainDiv.style.backgroundColor = "grey";
+            newMainDiv.style.borderColor = "#76bc43";
+            newMainDiv.style.backgroundColor = "#75bc43";
             newMainDiv.style.display = "block";
             newMainDiv.style.position = "relative";
-            newMainDiv.style.top = "200px";
+            newMainDiv.style.marginTop = "10%";
+            newMainDiv.style.marginLeft = "5%"
             newMainDiv.id = "newMainDiv";
       
             let refDiv = document.getElementById("container-end");
             let anonyDiv = refDiv.parentNode;
             anonyDiv.insertBefore(newMainDiv, refDiv);
+
+            //insertion ref div
+            let gameRefDiv = document.createElement("div");
+            newMainDiv.appendChild(gameRefDiv);
+
+
+            //Create a text area
+            let textArea = document.createElement("textarea");
+            textArea.rows = 3;
+            textArea.cols = 99;
+            textArea.maxLength = 300;
+            textArea.style.fontSize = "24px";
+            textArea.style.fontFamily = "Mali";
+            textArea.style.resize = "none";
+            textArea.style.width = "80%";
+            textArea.style.height = "33%";
+            textArea.style.borderRadius = "1px";
+            textArea.style.borderStyle = "solid";
+            textArea.style.borderColor = "#febd3d";
+            textArea.style.backgroundColor = "grey";
+            textArea.style.display = "inline";
+            textArea.style.position = "relative";
+            textArea.style.top = "300px";
+            textArea.style.marginLeft = "10%"
+            textArea.id = "textArea";
+      
+            newMainDiv.insertBefore(textArea, gameRefDiv);
+
+
+
+            //Create a title
+            let titleArea = document.createElement("div");
+
+            titleArea.style.fontSize = "36px";
+            titleArea.style.fontWeight = "700";
+            titleArea.style.fontFamily = "Mali";
+            titleArea.style.width = "80%";
+            titleArea.style.height = "auto";
+            titleArea.style.color = "#1e4488";
+            titleArea.style.display = "inline";
+            titleArea.style.position = "absolute";
+            titleArea.style.top = "10px";
+            titleArea.style.marginLeft = "10%"
+            titleArea.style.textAlign = "center";
+            titleArea.id = "titleArea";
+            titleArea.innerHTML = "PlaceHolder Title";
+      
+            newMainDiv.appendChild(titleArea);
+
       
            
       
@@ -63,7 +114,7 @@ async function fetchInfo () {
               
               var objectURL = myBlob;
               let newMainDiv = document.getElementById("newMainDiv");
-              newMainDiv.innerHTML = objectURL;
+              //newMainDiv.innerHTML = objectURL;
               return objectURL;
               
             })
@@ -89,4 +140,12 @@ async function fetchInfo () {
 localFunc.setScene();
 
 var testVar100 = fetchInfo();
+
+var testText = document.getElementById("textArea");
+
+testText.addEventListener("input",function(){
+      var text = testText.value;
+      text = text.toString();
+      document.getElementById("titleArea").innerHTML = text;
+},false);
 
